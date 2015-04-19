8b98c6ac42c9016441a8bd619d7749f808a86f74
Daved <daved@codingmusica.com>
2015-04-19 12:44:47 -0700

    Moved Set work into a case and removed mutex.  It should still be thread-safe, and it should be able to be called from within provided reload and stop functions.

--- sigmon.go
+++ sigmon.go
@@ -5,34 +5,39 @@ package sigmon
 import (
 	"os"
 	"os/signal"
-	"sync"
 	"syscall"
 )
 
 // SignalMonitor holds and calls funcs when called by relevant signals.
 type SignalMonitor struct {
-	mx     sync.RWMutex
 	reload func()
 	stop   func()
 	sig    string
 	isOn   bool
 	off    chan bool
+	set    chan sigFuncs
+}
+
+type sigFuncs struct {
+	reload func()
+	stop   func()
 }
 
 // New takes a reload and stop function and returns a set SignalMonitor.
 // When a nil arg is provided, no action will be taken during the relevant
 // signal.  Run must be called in order to begin monitoring.
 func New(reload, stop func()) (s *SignalMonitor) {
-	s = &SignalMonitor{reload: reload, stop: stop}
+	s = &SignalMonitor{reload: reload, stop: stop,
+		off: make(chan bool), set: make(chan sigFuncs)}
 	return s
 }
 
 // Set allows functions to be added or removed.
 func (sm *SignalMonitor) Set(reload, stop func()) {
-	sm.mx.Lock()
-	sm.reload = reload
-	sm.stop = stop
-	sm.mx.Unlock()
+	fns := sigFuncs{reload: reload, stop: stop}
+	go func() {
+		sm.set <- fns
+	}()
 }
 
 // Run starts signal monitoring.  If functions have been provided, they will
@@ -42,10 +47,6 @@ func (sm *SignalMonitor) Set(reload, stop func()) {
 // a default behavior of either INT or TERM.
 func (sm *SignalMonitor) Run() {
 	if !sm.isOn {
-		if sm.off == nil {
-			sm.off = make(chan bool)
-		}
-
 		go func(s *SignalMonitor) {
 			h := make(chan os.Signal, 1)
 			i := make(chan os.Signal, 1)
@@ -56,26 +57,23 @@ func (sm *SignalMonitor) Run() {
 
 			for {
 				select {
+				case fns := <-s.set:
+					s.reload = fns.reload
+					s.stop = fns.stop
 				case <-h:
 					s.sig = "HUP"
 					if s.reload != nil {
-						s.mx.RLock()
 						s.reload()
-						s.mx.RUnlock()
 					}
 				case <-i:
 					s.sig = "INT"
 					if s.stop != nil {
-						s.mx.RLock()
 						s.stop()
-						s.mx.RUnlock()
 					}
 				case <-t:
 					s.sig = "TERM"
 					if s.stop != nil {
-						s.mx.RLock()
 						s.stop()
-						s.mx.RUnlock()
 					}
 				case <-s.off:
 					s.isOn = false
