9c95d3a07c59ca74b989a161558fd90871f08221
Daved <daved@codingmusica.com>
2015-04-18 23:35:41 -0700

    Renamed struct function fields and reload/stop function args, and also renamed isRunning to isOn.

--- sigmon.go
+++ sigmon.go
@@ -11,27 +11,27 @@ import (
 
 // SignalMonitor holds and calls funcs when called by relevant signals.
 type SignalMonitor struct {
-	mx        sync.RWMutex
-	rFunc     func()
-	sFunc     func()
-	sig       string
-	isRunning bool
-	kill      chan bool
+	mx   sync.RWMutex
+	rel  func()
+	stop func()
+	sig  string
+	isOn bool
+	kill chan bool
 }
 
 // New takes a reload and stop function and returns a set SignalMonitor.
 // When a nil arg is provided, no action will be taken during the relevant
 // signal(s).  Run must be called in order to begin monitoring.
-func New(reloadFunc, stopFunc func()) (s *SignalMonitor) {
-	s = &SignalMonitor{rFunc: reloadFunc, sFunc: stopFunc}
+func New(reload, stop func()) (s *SignalMonitor) {
+	s = &SignalMonitor{rel: reload, stop: stop}
 	return s
 }
 
 // Set allows functions to be added or removed to increase flexibility.
-func (sm *SignalMonitor) Set(reloadFunc, stopFunc func()) {
+func (sm *SignalMonitor) Set(reload, stop func()) {
 	sm.mx.Lock()
-	sm.rFunc = reloadFunc
-	sm.sFunc = stopFunc
+	sm.rel = reload
+	sm.stop = stop
 	sm.mx.Unlock()
 }
 
@@ -39,7 +39,7 @@ func (sm *SignalMonitor) Set(reloadFunc, stopFunc func()) {
 // be called during the relevant case.  The os.Signal which was called will
 // also be stored as a string within the SignalMonitor for retrieval (GetLast).
 func (sm *SignalMonitor) Run() {
-	if !sm.isRunning {
+	if !sm.isOn {
 		sm.kill = make(chan bool)
 		go func(s *SignalMonitor) {
 			h := make(chan os.Signal, 1)
@@ -52,31 +52,31 @@ func (sm *SignalMonitor) Run() {
 				select {
 				case <-h:
 					s.sig = "HUP"
-					if s.rFunc != nil {
+					if s.rel != nil {
 						s.mx.RLock()
-						s.rFunc()
+						s.rel()
 						s.mx.RUnlock()
 					}
 				case <-i:
 					s.sig = "INT"
-					if s.sFunc != nil {
+					if s.stop != nil {
 						s.mx.RLock()
-						s.sFunc()
+						s.stop()
 						s.mx.RUnlock()
-						s.isRunning = false
+						s.isOn = false
 						return
 					}
 				case <-t:
 					s.sig = "TERM"
-					if s.sFunc != nil {
+					if s.stop != nil {
 						s.mx.RLock()
-						s.sFunc()
+						s.stop()
 						s.mx.RUnlock()
-						s.isRunning = false
+						s.isOn = false
 						return
 					}
 				case <-s.kill:
-					s.isRunning = false
+					s.isOn = false
 					return
 				}
 			}
@@ -86,7 +86,7 @@ func (sm *SignalMonitor) Run() {
 
 // Stop kills the goroutine which is monitoring signals.
 func (sm *SignalMonitor) Stop() {
-	if sm.isRunning && sm.kill != nil {
+	if sm.isOn && sm.kill != nil {
 		sm.kill <- true
 	}
 }
