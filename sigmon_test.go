95ea8d314e304bd1dd61a5c66a4276bc335dc137
Daved <daved@codingmusica.com>
2015-04-19 01:05:38 -0700

    Added examples to godoc.

---
+++ sigmon_test.go
@@ -0,0 +1,49 @@
+package sigmon
+
+import (
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/codemodus/sigmon"
+)
+
+var (
+	reloadFunc func()
+	stopFunc   func()
+	mu         sync.Mutex
+	sm         *SignalMonitor
+)
+
+func Example() {
+	sm := sigmon.New(nil, nil)
+	sm.Run()
+
+	// Do things which cannot be affected by OS signals...
+
+	sm.Set(reloadFunc, stopFunc)
+
+	// Do things which can be affected by OS signals...
+
+	sm.Stop()
+
+	// OS signals will be handled normally.
+}
+
+func Example_reloadFunc() {
+	t1 := time.Now()
+	mu.Lock()
+	// Reload config
+	mu.Unlock()
+	t2 := time.Now()
+	fmt.Println(sm.GetLast(), t2.Sub(t1))
+	// Output: HUP 156.78µs
+}
+
+func Example_stopFunc() {
+	// Only handle TERM signals.
+	if sm.GetLast() == "TERM" {
+		fmt.Println(sm.GetLast())
+	}
+	// Output: TERM
+}
