b8091b5e03844e3534a3a967da7b7ca4469b6635
Daved <daved@codemodus.com>
2015-05-07 02:23:08 -0700

    More changed in example functions.

--- sigmon_test.go
+++ sigmon_test.go
@@ -7,43 +7,40 @@ import (
 	"github.com/codemodus/sigmon"
 )
 
-var (
-	sm *sigmon.SignalMonitor
-)
-
 func ExampleSignalMonitor() {
-	sm := sigmon.New(nil)
-	sm.Run()
+	myServer.sm := sigmon.New(nil)
+	myServer.sm.Run()
 	// Do things which cannot be affected by OS signals...
 
-	sm.Set(myServer.signalHandler)
+	myServer.sm.Set(myServer.signalHandler)
 	// Do things which can be affected by OS signals...
 
-	sm.Set(nil)
+	myServer.sm.Set(nil)
 	// Do more things which cannot be affected by OS signals...
 
-	sm.Stop()
+	myServer.sm.Stop()
 	// OS signals will be handled normally.
 }
 
 func ExampleSignalMonitor_signalHandlerFunc() {
+	// Within (ms *myServer) myServer.signalHandler().
 	switch sm.Sig() {
 	case sigmon.SIGHUP:
-		myServer.reload(false)
+		ms.reload(false)
 	case sigmon.SIGINT, sigmon.SIGTERM:
-		myServer.stop()
+		ms.stop()
 	case sigmon.SIGUSR1:
-		myServer.reload(true)
+		ms.reload(true)
 	case sigmon.SIGUSR2:
 		fmt.Println("USR2")
 	}
 
-	// ... within the reload function.
+	// Within (ms *myServer) myServer.reload(reconfig bool).
 	t1 := time.Now()
-	myServer.Lock()
-	// Reload config.
-	myServer.Unlock()
+	if reconfig {
+		// Reload config.
+	}
 	t2 := time.Now()
-	fmt.Println(sm.Sig(), t2.Sub(t1))
+	fmt.Println(ms.sm.Sig(), t2.Sub(t1))
 	// Output: HUP 156.78µs
 }
