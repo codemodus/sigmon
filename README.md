0ee4131710a2e2e3b16968789df832891ca0e32f
Daved <daved@codemodus.com>
2015-05-30 00:31:20 -0700

    Reworked test file examples, added tests (100% coverage), and added benchmark to show allocations.  Added much to readme.

--- README.md
+++ README.md
@@ -1,9 +1,94 @@
 # sigmon
---
-    import "github.com/codemodus/sigmon"
+
+    go get "github.com/codemodus/sigmon"
 
 Package sigmon simplifies os.Signal handling.
 
 ## Usage
 
+```go
+type Signal
+type SignalMonitor
+    func New(handler func(*SignalMonitor)) (s *SignalMonitor)
+    func (s *SignalMonitor) Run()
+    func (s *SignalMonitor) Set(handler func(*SignalMonitor))
+    func (s *SignalMonitor) Sig() Signal
+    func (s *SignalMonitor) Stop()
+```
+
+### Setup
+
+```go
+import (
+    "github.com/codemodus/sigmon"
+)
+
+func main() {
+    sm := sigmon.New(nil)
+    sm.Run()
+    // Do things which cannot be affected by OS signals...
+
+    sm.Set(signalHandler)
+    // Do things which can be affected by OS signals...
+
+    sm.Set(nil)
+    // Do more things which cannot be affected by OS signals...
+
+    sm.Stop()
+    // OS signals will be handled normally.
+}
+```
+
+### Signal Handler
+
+```go
+func signalHandler(sm *sigmon.SignalMonitor) {
+    switch sm.Sig() {
+        switch sm.Sig() {
+        case sigmon.SIGHUP:
+            // Reload
+        case sigmon.SIGINT, sigmon.SIGTERM:
+            // Stop
+        case sigmon.SIGUSR1, sigmon.SIGUSR2:
+            // More
+        }
+    }
+}
+```
+
+### Signal Handler With Context
+
+```go
+func main() {
+    ctxWrap := &contextWrap{c: make(chan string), prefix: "called/wrapped - "}
+    sm := sigmon.New(ctxWrap.prefixAndLowerCaseHandler)
+    sm.Run()
+    
+    // Simulate system signal calls and print results.
+    callOSSiganl(syscall.SIGINT)
+    select {
+    case result := <-ctxWrap.c:
+        fmt.Println(result) // Output: "called/wrapped - int"
+    case <-time.After(1 * time.Second):
+        fmt.Println("timeout waiting for signal")
+    }
+    
+    sm.Stop()
+}
+```
+
+## More Info
+
+N/A
+
+## Documentation
+
 View the [GoDoc](http://godoc.org/github.com/codemodus/sigmon)
+
+## Benchmarks
+
+This result is to highlight allocations.
+
+    benchmark           iter      time/iter   bytes alloc         allocs
+    ---------           ----      ---------   -----------         ------
+    BenchmarkSimple   200000    10.69 μs/op     1398 B/op   28 allocs/op
--- sigmon_test.go
+++ sigmon_test.go
@@ -2,70 +2,198 @@ package sigmon_test
 
 import (
 	"fmt"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+	"testing"
 	"time"
 
 	"github.com/codemodus/sigmon"
 )
 
-var (
-	myServer = &server{}
-)
-
-type server struct {
-	sm *sigmon.SignalMonitor
+type contextWrap struct {
+	c      chan string
+	prefix string
 }
 
-func (s *server) signalHandler()           {}
-func (s *server) reload(withReconfig bool) {}
-func (s *server) stop()                    {}
+func Example() {
+	sm := sigmon.New(nil)
+	sm.Run()
+	// Do things which cannot be affected by OS signals...
 
-func ExampleSignalMonitor() {
-	main := func() {
-		myServer.sm = sigmon.New(nil)
-		myServer.sm.Run()
-		// Do things which cannot be affected by OS signals...
+	sm.Set(signalHandler)
+	// Do things which can be affected by OS signals...
 
-		myServer.sm.Set(myServer.signalHandler)
-		// Do things which can be affected by OS signals...
+	sm.Set(nil)
+	// Do more things which cannot be affected by OS signals...
 
-		myServer.sm.Set(nil)
-		// Do more things which cannot be affected by OS signals...
-
-		myServer.sm.Stop()
-		// OS signals will be handled normally.
-	}
-	main()
+	sm.Stop()
+	// OS signals will be handled normally.
 }
 
-func ExampleSignalMonitor_signalHandlerFunc() {
-	// contents of a func (s *server) signalHandler() {}
-	// formatted to satisfy go test
-	myServer.sm = sigmon.New(nil)
-	signalHandler := func() {
-		switch myServer.sm.Sig() {
+func Example_signalHandlerFunc() {
+	// ...
+
+	signalHandler := func(sm *sigmon.SignalMonitor) {
+		switch sm.Sig() {
 		case sigmon.SIGHUP:
-			myServer.reload(false)
+			// Reload
 		case sigmon.SIGINT, sigmon.SIGTERM:
-			myServer.stop()
-		case sigmon.SIGUSR1:
-			myServer.reload(true)
-		case sigmon.SIGUSR2:
-			fmt.Println("USR2")
+			// Stop
+		case sigmon.SIGUSR1, sigmon.SIGUSR2:
+			// More
 		}
 	}
-	signalHandler()
 
-	// contents of a func (s *server) reload(withReconfig bool) {}
-	// formatted to satisfy go test
-	reload := func(withReconfig bool) {
-		t1 := time.Now()
-		if withReconfig {
-			// Reload config.
-		}
-		t2 := time.Now()
-		fmt.Println(myServer.sm.Sig(), t2.Sub(t1))
-		// Output example: "HUP 156.78µs"
+	sm := sigmon.New(signalHandler)
+	sm.Run()
 
+	// ...
+}
+
+func Example_funWithContext() {
+	ctxWrap := &contextWrap{c: make(chan string), prefix: "called/wrapped - "}
+	sm := sigmon.New(ctxWrap.prefixAndLowerCaseHandler)
+	sm.Run()
+
+	// Simulate system signal calls and print results.
+	callOSSiganl(syscall.SIGINT)
+	select {
+	case result := <-ctxWrap.c:
+		fmt.Println(result)
+	case <-time.After(1 * time.Second):
+		fmt.Println("timeout waiting for signal")
 	}
-	reload(true)
+
+	callOSSiganl(syscall.SIGHUP)
+	select {
+	case result := <-ctxWrap.c:
+		fmt.Println(result)
+	case <-time.After(1 * time.Second):
+		fmt.Println("timeout waiting for signal")
+	}
+
+	sm.Stop()
+
+	// Output:
+	// called/wrapped - int
+	// called/wrapped - hup
+}
+
+func TestSignalIgnorance(t *testing.T) {
+	sm := sigmon.New(nil)
+	sm.Run()
+
+	testCallOSSiganl(t, syscall.SIGINT)
+
+	sm.Stop()
+}
+
+func TestSignalHandling(t *testing.T) {
+	cw := &contextWrap{c: make(chan string), prefix: "wrapped"}
+
+	tests := []struct {
+		h    func(*sigmon.SignalMonitor)
+		send syscall.Signal
+		recv string
+	}{
+		{cw.signalHandler, syscall.SIGHUP, string(sigmon.SIGHUP)},
+		{cw.signalHandler, syscall.SIGINT, string(sigmon.SIGINT)},
+		{cw.signalHandler, syscall.SIGTERM, string(sigmon.SIGTERM)},
+		{cw.signalHandler, syscall.SIGUSR1, string(sigmon.SIGUSR1)},
+		{cw.signalHandler, syscall.SIGUSR2, string(sigmon.SIGUSR2)},
+		{cw.prefixAndLowerCaseHandler, syscall.SIGHUP, cw.prefix + "hup"},
+		{cw.prefixAndLowerCaseHandler, syscall.SIGINT, cw.prefix + "int"},
+		{cw.prefixAndLowerCaseHandler, syscall.SIGTERM, cw.prefix + "term"},
+		{cw.prefixAndLowerCaseHandler, syscall.SIGUSR1, cw.prefix + "usr1"},
+		{cw.prefixAndLowerCaseHandler, syscall.SIGUSR2, cw.prefix + "usr2"},
+	}
+
+	sm := sigmon.New(nil)
+	sm.Run()
+
+	testCallOSSiganl(t, syscall.SIGINT)
+
+	for _, v := range tests {
+		sm.Set(v.h)
+
+		testCallOSSiganl(t, v.send)
+
+		want := v.recv
+		select {
+		case got := <-cw.c:
+			if got != want {
+				t.Errorf("signal was %v, want %v", got, want)
+			}
+		case <-time.After(1 * time.Second):
+			t.Errorf("timeout waiting for %v", want)
+		}
+	}
+}
+
+func signalHandler(sm *sigmon.SignalMonitor) {
+	switch sm.Sig() {
+	case sigmon.SIGHUP, sigmon.SIGINT, sigmon.SIGTERM, sigmon.SIGUSR1, sigmon.SIGUSR2:
+	}
+}
+
+func callOSSiganl(s syscall.Signal) {
+	c := make(chan os.Signal)
+	signal.Notify(c, s)
+	if err := syscall.Kill(syscall.Getpid(), s); err != nil {
+		fmt.Println(err)
+	}
+	select {
+	case <-c:
+		// prevent syscall.Kill from "bleeding"
+		time.Sleep(10 * time.Microsecond)
+	case <-time.After(1 * time.Second):
+		fmt.Printf("timeout waiting for %v", s)
+	}
+	signal.Stop(c)
+}
+
+func (cw *contextWrap) signalHandler(sm *sigmon.SignalMonitor) {
+	s := sm.Sig()
+	switch s {
+	case sigmon.SIGHUP, sigmon.SIGINT, sigmon.SIGTERM, sigmon.SIGUSR1, sigmon.SIGUSR2:
+		cw.c <- string(s)
+	}
+}
+
+func (cw *contextWrap) prefixAndLowerCaseHandler(sm *sigmon.SignalMonitor) {
+	switch sm.Sig() {
+	case sigmon.SIGHUP, sigmon.SIGINT, sigmon.SIGTERM, sigmon.SIGUSR1, sigmon.SIGUSR2:
+		g := cw.contextHandler(sm)
+		cw.c <- g
+	}
+}
+
+func (cw *contextWrap) contextHandler(sm *sigmon.SignalMonitor) string {
+	return cw.prefix + strings.ToLower(string(sm.Sig()))
+}
+
+func testCallOSSiganl(t *testing.T, s syscall.Signal) {
+	c := make(chan os.Signal)
+	signal.Notify(c, s)
+	if err := syscall.Kill(syscall.Getpid(), s); err != nil {
+		t.Fatal(err)
+	}
+	select {
+	case <-c:
+		// prevent syscall.Kill from "bleeding"
+		time.Sleep(10 * time.Microsecond)
+	case <-time.After(1 * time.Second):
+		t.Fatalf("timeout waiting for %v", s)
+	}
+	signal.Stop(c)
+}
+
+func BenchmarkSimple(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		sm := sigmon.New(nil)
+		sm.Run()
+		sm.Stop()
+	}
 }
