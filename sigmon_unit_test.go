fad9334da982a9b5df8e8f2597dda861207497e7
Daved <daved@codemodus.com>
2016-08-19 20:37:36 -0700

    Add signal handler set and handle unit tests.

--- sigmon_unit_test.go
+++ sigmon_unit_test.go
@@ -77,6 +77,29 @@ func TestUnitSignalHandlerRegister(t *testing.T) {
 	}
 }
 
+func TestUnitSignalHandlerSet(t *testing.T) {
+	c := &checkable{id: 123}
+	h := newSignalHandler(nil)
+	h.set(c.handler)
+
+	h.handler(&SignalMonitor{})
+
+	if c.id != c.val {
+		t.Errorf("want %d, got %d", c.id, c.val)
+	}
+}
+
+func TestUnitSignalHandlerHandle(t *testing.T) {
+	c := &checkable{id: 123}
+	h := newSignalHandler(c.handler)
+
+	h.handle(&SignalMonitor{})
+
+	if c.id != c.val {
+		t.Errorf("want %d, got %d", c.id, c.val)
+	}
+}
+
 func receiveOnAll(j *signalJunction) bool {
 	for i := 0; i < 5; i++ {
 		select {
