c01ace29c386c632cd52d392c15a591a10a63687
Daved <daved@codemodus.com>
2016-08-19 20:28:41 -0700

    Remove new signal junction unit test, add signal handler register unit test.

--- sigmon_unit_test.go
+++ sigmon_unit_test.go
@@ -16,23 +16,13 @@ var (
 	}
 )
 
-func TestUnitNewSignalJunction(t *testing.T) {
-	j := newSignalJunction()
-	if nil == j {
-		t.Fatal("wanted new signalJunction, got nil")
-	}
+type checkable struct {
+	id  int
+	val int
+}
 
-	go func() {
-		j.sighup <- syscall.SIGHUP
-		j.sigint <- syscall.SIGINT
-		j.sigterm <- syscall.SIGTERM
-		j.sigusr1 <- syscall.SIGUSR1
-		j.sigusr2 <- syscall.SIGUSR2
-	}()
-
-	if !receiveOnAll(j) {
-		t.Fatal("should not wait forever")
-	}
+func (c *checkable) handler(sm *SignalMonitor) {
+	c.val = c.id
 }
 
 func TestUnitSignalJunctionConnect(t *testing.T) {
@@ -57,6 +47,33 @@ func TestUnitSignalJunctionDisconnect(t *testing.T) {
 
 	if receiveOnAll(j) {
 		t.Fatal("should wait forever")
+	}
+}
+
+func TestUnitSignalHandlerRegister(t *testing.T) {
+	c1 := &checkable{id: 123}
+	c2 := &checkable{id: 234}
+
+	h := newSignalHandler(nil)
+	h.register(c1.handler)
+	h.register(c2.handler)
+
+	select {
+	case fn := <-h.registry:
+		if fn == nil {
+			t.Error("want function, got nil")
+		}
+
+		fn(&SignalMonitor{})
+	case <-time.After(time.Millisecond):
+		t.Error("should not wait forever")
+	}
+
+	if 0 != c1.val {
+		t.Errorf("want %d, got %d", 0, c1.val)
+	}
+	if c2.id != c2.val {
+		t.Errorf("want %d, got %d", c2.id, c2.val)
 	}
 }
 
