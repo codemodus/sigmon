ca952f4dda5b694a9bc687ad8cce3353353b6535
Daved <daved@codemodus.com>
2018-06-28 11:46:15 -0700

    Move state type into sigmon file.

--- sigmon.go
+++ sigmon.go
@@ -5,6 +5,23 @@ import (
 	"syscall"
 )
 
+// State holds information set by a corresponding signal.
+type State struct {
+	s Signal
+}
+
+func newState(s Signal) *State {
+	return &State{
+		s: s,
+	}
+}
+
+// Signal returns a representation of the system signal which spawned the
+// creation of the State instance.
+func (s *State) Signal() Signal {
+	return s.s
+}
+
 // HandlerFunc is used to communicate system signal handling behavior.
 type HandlerFunc func(*State)
 
